a914868f435b804ac4978f1d638799c159c52390
Marvin Scham <dev@marvinscham.de>
2022-06-17 06:51:49 +0200

    Added build command file

---
+++ build.cmd
@@ -0,0 +1,1 @@
+ocra disenchanter.rb --dll ruby_builtin_dlls\libssp-0.dll --dll ruby_builtin_dlls\libgmp-10.dll --dll ruby_builtin_dlls\libgcc_s_seh-1.dll --dll ruby_builtin_dlls\libwinpthread-1.dll --dll ruby_builtin_dlls\libssl-1_1-x64.dll --dll ruby_builtin_dlls\libcrypto-1_1-x64.dll
