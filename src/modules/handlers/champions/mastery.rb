34e2a1d76dc07a7cb0f219a00dd983d5868b8a42
marvinscham <dev@marvinscham.de>
2024-02-15 22:12:08 +0100

    No longer disenchants shards for champions at 0 mastery, fixes #87

--- src/modules/handlers/champions/mastery.rb
+++ src/modules/handlers/champions/mastery.rb
@@ -6,24 +6,23 @@ def handle_champions_mastery(client, loot_shards, keep_all: false)
   threshold_champion_ids = []
   mastery6_champion_ids = []
   mastery7_champion_ids = []
+  level_threshold = 0
 
-  level_threshold = if keep_all
-                      '0'
-                    else
-                      user_input_check(
-                        'Which mastery level should champions at least be for their shards to be kept?',
-                        %w[1 2 3 4 5 6],
-                        '[1..6]'
-                      )
-                    end
-  level_threshold = level_threshold.to_i
+  unless keep_all
+    level_threshold = user_input_check(
+      'Which mastery level should champions at least be for their shards to be kept?',
+      %w[1 2 3 4 5 6],
+      '[1..6]'
+    ).to_i
+  end
 
   player_mastery.each do |m|
-    if m['championLevel'] == 7
+    case m['championLevel']
+    when 7
       mastery7_champion_ids << m['championId']
-    elsif m['championLevel'] == 6
+    when 6
       mastery6_champion_ids << m['championId']
-    elsif keep_all || ((level_threshold..5).include? m['championLevel'])
+    when level_threshold..5
       threshold_champion_ids << m['championId']
     end
   end
@@ -34,7 +33,7 @@ def handle_champions_mastery(client, loot_shards, keep_all: false)
     elsif mastery6_champion_ids.include? l['storeItemId']
       l['count'] -= 1
       l['count_keep'] += 1
-    elsif threshold_champion_ids.include? l['storeItemId']
+    elsif keep_all || (threshold_champion_ids.include? l['storeItemId'])
       l['count'] -= 2
       l['count_keep'] += 2
     else
